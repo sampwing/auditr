df30fe2a0d95718fe0f88efd97db98f0ba0d4655
Sam Wing <sampwing@gmail.com>
2014-10-29 17:57:10 -0700

    create configuration file and try to parse

--- src/auditr/core.clj
+++ src/auditr/core.clj
@@ -5,6 +5,7 @@
 (require '[amazonica.core :as amazonica])
 (require '[amazonica.aws.ec2 :as ec2])
 (require '[auditr.util :as util])
+(require '[auditr.config :as config])
 
 (defn aws-authenticate [& args]
   (amazonica/defcredential (System/getenv "AWS_AUDIT_KEY") (System/getenv "AWS_AUDIT_SECRET")))
@@ -19,16 +20,40 @@
   "print security-group information"
   [sg]
   (let [{:keys [description group-name ip-permissions]} sg]
-    (println "")
-    (println (clojure.string/join " - " [(util/build-line {:group group-name}) description]))
     (let [m {:group group-name 
-             :rules (reduce concat (map build-config ip-permissions))}]
-      (prn m))))
+             :rules (sort (reduce concat (map build-config ip-permissions)))}]
+      m)))
  
-(aws-authenticate)
 
-(def sgs 
-  (:security-groups (ec2/describe-security-groups)))
+(defn get-security-group-rules
+  []
+  (aws-authenticate)
+  (let [{security-groups :security-groups} (ec2/describe-security-groups)]
+    (let [rules (sort #(compare (:group %1) (:group %2)) (map sg-info security-groups))]
+      rules)))
+
+(defn generate-configuration-body-helper
+  [rule]
+  (str (util/build-line rule) "\n" (clojure.string/join "\n" (:rules rule))))
+
+(defn generate-configuration-body
+  []
+  (let [rules (get-security-group-rules)]
+    (map generate-configuration-body-helper rules)))
+
+(defn generate-configuration
+  [filename]
+  (let [rules (get-security-group-rules)]
+    (spit filename (clojure.string/join "\n" (generate-configuration-body)))))
+
+(defn parse-configuration
+  [filename]
+  (let [lines (take 10 (clojure.string/split (slurp filename) #"\n"))]
+    (prn lines)
+    (let [parsed-lines (map config/config-parse-line lines)]
+      (prn parsed-lines)
+      )))
+
 
 (def cli-options
   [["-o" "--output OUTPUT" "Output File"
@@ -39,8 +64,8 @@
   [& args]
   (let [{options :options} (parse-opts args cli-options)]
     (doseq [arg args] 
-      (if (and 
-            (= arg "generate")
-            ((complement nil?) (:output options)))
-        (doseq [sg sgs] (sg-info sg))))))
+      (if (= arg "generate")
+        (generate-configuration "configuration")
+        (if (= arg "parse")
+          (prn (parse-configuration "configuration")))))))
 
